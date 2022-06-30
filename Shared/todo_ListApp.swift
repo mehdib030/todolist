1025e821333e9538675a6a7990f71a6e36d9bce9
mehdibenyebka <mehdib030@gmail.com>
2022-06-29 21:57:16 -0500

    Initial Commit

---
+++ Shared/todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_ListApp.swift
+//  Shared
+//
+//  Created by Mehdi Benyebka on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
